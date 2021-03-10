ea5c4325c5bf6b470c8a04f02a9ec70768a4b808
Anton Zinakov <ab.zinakov@gmail.com>
2021-03-10 23:17:20 +0300

    Add build.cmd

---
+++ backend/build.cmd
@@ -0,0 +1,15 @@
+@REM TODO: Check
+
+set PROTO_BIN="../vcpkg/buildtrees/protobuf/x64-windows-rel/protoc.exe"
+set GRPC_CPP_PLUGIN="../vcpkg/buildtrees/grpc/x64-windows-rel/grpc_cpp_plugin.exe"
+set SRC_DIR=../proto
+set DST_DIR=generated
+
+%PROTO_BIN% -I=%SRC_DIR% --grpc_out=%DST_DIR% --plugin=protoc-gen-grpc=%GRPC_CPP_PLUGIN% %SRC_DIR%/helloworld.proto
+%PROTO_BIN% -I=%SRC_DIR% --cpp_out=%DST_DIR% %SRC_DIR%/helloworld.proto
+
+mkdir build
+pushd build
+cmake -G "Visual Studio 16 2019" -A x64 -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake ..
+make
+popd
